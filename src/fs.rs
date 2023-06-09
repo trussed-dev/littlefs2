800bccf3758379356d915aea5264a0d5e0465799
Sosthène Guédon <sosthene@nitrokey.com>
2023-06-09 17:18:13 +0200

    Bump littlefs2-sys

--- src/fs.rs
+++ src/fs.rs
@@ -132,6 +132,7 @@ impl<Storage: driver::Storage> Allocation<Storage> {
             name_max: filename_max_plus_one.wrapping_sub(1),
             file_max,
             attr_max,
+            metadata_max: 0,
         };
 
         Self {
