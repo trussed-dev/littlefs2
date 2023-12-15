b72fc22810559dd6b98f20760f2e79578e3d8bcf
robin-nitrokey <robin-nitrokey@users.noreply.github.com>
2023-12-15 15:49:46 +0000

    deploy: ebd27e49ca321089d01d8c9b169c4aeb58ceeeca

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"littlefs2":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.73.0/core/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"littlefs2/path/struct.Path.html\" title=\"struct littlefs2::path::Path\">Path</a>"]]
+"littlefs2":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.74.1/core/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"littlefs2/path/struct.Path.html\" title=\"struct littlefs2::path::Path\">Path</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/iter/traits/marker/trait.FusedIterator.js
+++ implementors/core/iter/traits/marker/trait.FusedIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"littlefs2":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"littlefs2/path/struct.Ancestors.html\" title=\"struct littlefs2::path::Ancestors\">Ancestors</a>&lt;'a&gt;"]]
+"littlefs2":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"littlefs2/path/struct.Ancestors.html\" title=\"struct littlefs2::path::Ancestors\">Ancestors</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
