2870252fa4e79f251b6112f4e9a1a77a2d9bbdf3
sosthene-nitrokey <sosthene-nitrokey@users.noreply.github.com>
2024-03-19 13:08:38 +0000

    deploy: 960e57d9fc0d209308c8e15dc26252bbe1ff6ba8

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"littlefs2":["",[["io",[],["prelude.rs"]]],["consts.rs","driver.rs","fs.rs","io.rs","lib.rs","macros.rs","object_safe.rs","path.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["littlefs2",["",[["io",[],["prelude.rs"]]],["consts.rs","driver.rs","fs.rs","io.rs","lib.rs","macros.rs","object_safe.rs","path.rs"]]]\
+]'));
 createSrcSidebar();
